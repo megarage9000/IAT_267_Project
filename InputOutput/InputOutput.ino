c756b1ba2ff90a1b9a06148d8077ff1c9f973b73
John <gtom_1984@hotmail.ca>
2021-11-25 21:44:27 -0800

    initial commit of sensor reading
    - test on dummy processing file

--- InputOutput/InputOutput.ino
+++ InputOutput/InputOutput.ino
@@ -1,108 +1,73 @@
 #include <Wire.h>                 // Must include Wire library for I2C
+#include <Servo.h>
 #include "SparkFun_MMA8452Q.h"    // Click here to get the library: http://librarymanager/All#SparkFun_MMA8452Q
+
 MMA8452Q accel;                   // create instance of the MMA8452 class
+Servo ticker;
 
-// Set Analog inputs (Any analog inputs)
-int forcePin = A0;
-int potPin = A1;
-int accelPin = -2;
-int noInput = -1;
+// analog pins
+int potenioPin = A0;
+int forcePin = A1;
+int servoPin = A2;
 
-// Initialize sensor values
-int inputPin = accelPin;
-int inputVal = 0;
-int delayTime = 0;
-
+// divider
+char divider = '|';
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  //accelerometer setup
+  ticker.attach(servoPin);
   accel.init();
-//  while (accel.begin() == false) {
-//    Serial.println("Not Connected. Please check connections and read the hookup guide.");
-//    //while (1);
-//  } 
+  Serial.begin(9600);
 }
 
-void loop() {
-//  Serial.println(inputPin);
-  // put your main code here, to run repeatedly:
-//  if(Serial.available() > 0) {
-//    String keyToRead = Serial.readString();
-//    if(inputPin != noInput) {
-//      
-//    }
-//  }
-  //Accelerometer Code
-  sendSensorInput();
-}
 
-void readSensor(int pin) {
-  int returnVal = analogRead(pin);
-  Serial.println(returnVal);
-  delay(delayTime);
-}
-
-void stringToAction(String message) {
-  if(message == "potentioSet") {
-    inputPin = potPin;
-    delayTime = 100;
-  }
-  else if(message == "forceSet") {
-    inputPin = forcePin;
-    delayTime = 100;
-  }
-  else if(message == "accelSet") {
-    inputPin = accelPin;
-    delayTime = 100;
-  }
-  else if(message == "reset") {
-    inputPin =  noInput;
-    delayTime = 100;
-  }
-  else if(message == "gamePass") {
-    // Need to figure out function for calling game pass
-    inputPin =  noInput;    
-  }
-  else if(message == "gameFail") {
-     // Need to figure out function for calling game fail
-    inputPin =  noInput;  
-  }
-  else{
-    inputPin = noInput;
-  }
-}
-
-void sendSensorInput() {
-  if(inputPin == accelPin) {
-    accelerometerRead();
+void loop() { 
+  if(Serial.available()) {
+    Serial.println(Serial.readString());
   }
   else {
-    int val = analogRead(inputPin);
-    Serial.println(val);
-    delay(delayTime);
+    String inputVals = packageInputVals();
+    Serial.println(inputVals);
+    delay(100);
   }
 }
 
-void accelerometerRead() {
+String packageInputVals() {
+  // sensor values
+  String inputPackageVals = "";
+  // -- Read Accelerometer
+  inputPackageVals = accelerometerRead();
+  inputPackageVals += divider;
+  // -- Read Poteniometer
+  inputPackageVals += analogRead(potenioPin);
+  inputPackageVals += divider;
+  // -- Read Light / Force Sensor
+  inputPackageVals += analogRead(forcePin);
+  return inputPackageVals;
+}
+
+String accelerometerRead() {
   if (accel.available()) {      // Wait for new data from accelerometer
     // Orientation of board (Right, Left, Down, Up);
     if (accel.isRight() == true) {
-      Serial.println("Right");
+      return "Right";
     }
     else if (accel.isLeft() == true) {
-      Serial.println("Left");
+      return "Left";
     }
     else if (accel.isUp() == true) {
-      Serial.println("Up");
+      return "Up";
     }
     else if (accel.isDown() == true) {
-      Serial.println("Down");
+      return "Down";
     }
     else if (accel.isFlat() == true) {
-      Serial.println("Flat");
+      return "Flat";
+    }
+    else {
+      return "None";
     }
   }
-  delay(delayTime);
+  else {
+    return "None";
+  }
 }
